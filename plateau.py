b895f7213b4880fe70b1d899f822bea5c514c842
XNarno <arnaudnizet85@gmail.com>
2021-05-16 20:25:35 +0200

    Plus de constantes et tests

--- plateau.py
+++ plateau.py
@@ -2,29 +2,45 @@ import math
 import pygame
 import sys
 import os
+import pyautogui
 
 # Constantes
+HD = (1280, 720)
+FULLHD = (1920, 1080)
+QHD = (2560, 1440)
+FOUR_K = (3840, 2160)
+EIGHT_K = (7680, 4320)
 
+LOW_FPS = 30
+FULL_FPS = 60
+
+NOIR = (0, 0, 0)
+BLANC = (255, 255, 255)
 BLEUCLAIR = (127, 191, 255)
 
 # Paramètres
 
-dimensions_fenetre = (1600, 900)  # en pixels
-images_par_seconde = 25
+dimensions_fenetre = HD  # en pixels
+images_par_seconde = LOW_FPS
+
+# Fonctions
+
+def actualisation():
+    fenetre.fill(BLEUCLAIR)
+    pygame.draw.circle(fenetre, NOIR, pygame.mouse.get_pos(), 30, 0)
 
 # Initialisation
 
 pygame.init()
 
 fenetre = pygame.display.set_mode(dimensions_fenetre)
-pygame.display.set_caption("Programme 1")
+pygame.display.set_caption("ResearchPhoenix")
 
 horloge = pygame.time.Clock()
-couleur_fond = BLEUCLAIR
 
 # Dessin
 
-fenetre.fill(couleur_fond)
+fenetre.fill(BLEUCLAIR)
 
 while True:
     for evenement in pygame.event.get():
@@ -32,5 +48,6 @@ while True:
             pygame.quit()
             sys.exit()
 
+    actualisation()
     pygame.display.flip()
     horloge.tick(images_par_seconde)
